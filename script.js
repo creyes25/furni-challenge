5097fabe95b36024bb962a094c003da60af9278b
creyes25 <carolina.reyes2022@gmail.com>
2023-09-28 01:03:54 -0500

    add hover animations throught website

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//   $('.carousel').slick({
+//     // dots: true,
+//     infinite: true,
+//     arrows: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//   });
+// });
